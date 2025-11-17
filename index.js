7c5fc1bb9ce5d0ca7df8839c2301c274b6cb333c
Sakif Ahmed <ahmed.sakif225@gmail.com>
2025-11-17 12:44:22 +0600

    Vercel Server issue

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.get('/', (req, res) => {
 
 async function run() {
     try {
-        await client.connect()
+        // await client.connect()
 
         const db = client.db("better_tomorrow_DB")
         const eventsCollection = db.collection("events")
